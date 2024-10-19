5e8ceeb26360369fe63edd73318c7d3c3d48e649
sinoosoida <bokhonko.ma@phystech.edu>
2024-10-20 02:22:10 +0300

    New implementation of data transferring debug

--- utils/deiterator.py
+++ utils/deiterator.py
@@ -15,8 +15,5 @@ class DeiteratorHandler(BaseHandler):
 
     def process(self, data:ImmutableDataChain):
         iterator = data.get_data("output_audio_iterator")
-        logger.debug(f"""Got iterator out of text {data.get_data("llm_sentence")} """)
         for chunk in iterator:
-            logger.debug(f"""Got a chunk out of text {data.get_data("llm_sentence")} """)
             yield data.add_data(chunk, "output_audio_chunk")
-        logger.debug(f"""Got all chuncks of iterator out of text {data.get_data("llm_sentence")} """)
