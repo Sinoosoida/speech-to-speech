33931fb1a2c3432bb2b5b761de558761455c4770
sinoosoida <bokhonko.ma@phystech.edu>
2024-10-25 04:19:46 +0300

    Refactor

--- baseHandler.py
+++ baseHandler.py
@@ -69,11 +69,13 @@ class BaseHandler:
             for chunk in self.process(input_data):
 
                 current_time = perf_counter()
+
                 if first_chunk_time is None:
                     first_chunk_time = current_time
                     total_time_first_chunk = first_chunk_time - start_time
-                    logger.debug(
-                        f"{self.__class__.__name__} [{writer_id}]: First chunk after {total_time_first_chunk:.3f} s")
+                    if total_time_first_chunk > self.min_time_to_debug:
+                        logger.debug(
+                            f"{self.__class__.__name__} [{writer_id}]: First chunk after {total_time_first_chunk:.3f} s")
 
                 with self.condition:
                     if writer_id == self.next_write_sequence:
@@ -85,7 +87,8 @@ class BaseHandler:
 
             last_chunk_time = perf_counter()
             total_time_all_chunks = last_chunk_time - start_time
-            logger.debug(f"{self.__class__.__name__} [{writer_id}]: All chunks after {total_time_all_chunks:.3f} s")
+            if total_time_all_chunks > self.min_time_to_debug:
+                logger.debug(f"{self.__class__.__name__} [{writer_id}]: All chunks after {total_time_all_chunks:.3f} s")
             #Получили все данные, но ждём очереди записи.
             if buffer:
                 with self.condition:
